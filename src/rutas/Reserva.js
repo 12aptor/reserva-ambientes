656c8bccc477b31b24689470ca9f77fbb6240201
Paolo Soncco <paholo2054@gmail.com>
2020-02-14 19:00:37 -0500

    agregue get reserva

--- src/rutas/Reserva.js
+++ src/rutas/Reserva.js
@@ -5,6 +5,7 @@ const {wachiman} = require('../utils/utils')
 
 reserva_router.post('/reservar',wachiman, Reservas.postReserva);
 reserva_router.get('/reservas/:fecha_inicio/:fecha_fin', Reservas.getReservaByFechas);
+reserva_router.get('/reserva', Reservas.getReservas);
 reserva_router.get('/reserva/:fecha_inicio/:fecha_fin/:ambiente',Reservas.validarReserva);
 
 module.exports = {
